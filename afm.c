78a874dad2c4f09bc802d648242939b5652acb74
Valter António <valter.antonio@gmail.com>
2014-11-12 17:15:32 +0000

    limpeza da pasta AFM

--- afm.c
+++ afm.c
@@ -8,24 +8,58 @@
 
 // Definição das estruturas de dados
 
-#define J 101
+#define J 101 // definição de constante global para nº jogadores
+#define E 101 // definição de constante global para nº equipas
+#define R 101 // definição de constante global para nº resultados
 
 typedef struct{
 
     int num_cc;
-    char nome[50];
-    char morada[50];
+    char nome[80];
+    char morada[80];
     char telefone[9];
     int idade;
     int ano_entrada;
-    char pos[20];
+    char pos[50];
     int ativo;
 
-} jogador;
+} jogador; // estrutura para variável do tipo jogador
 
-typedef jogador listajog[J];
+typedef jogador listajog[J]; // array de estruturas tipo jogador tamanho J
 
-listajog jog;
+listajog jog; // variável do tipo array
+
+typedef struct{
+
+    int id;
+    char nome[80];
+    int lista[26]; // array inteiro que guarda o [id] de 25 jogadores (1-25)
+    int ativo;
+
+} equipa; // estrutura para variável do tipo equipa
+
+typedef equipa listaequip[E]; // array de estruturas tipo jogador tamanho E
+
+listaequip equip; // variável do tipo array
+
+typedef struct{
+
+    int id;
+    char equipa_a[80];
+    char equipa_b[80];
+    int golos_a;
+    int golos_b;
+    int marcadores[21]; // array inteiro que guarda o [id] de 20 marcadores (1-20)
+    char data[10];
+    char local[80];
+    int ativo;
+
+} resultado; // estrutura para variável do tipo resultado
+
+typedef resultado listaresult[R]; // array de estruturas tipo resultado tamanho R
+
+listaresult result; // variável do tipo array
+
 
 #include "menu_1.c"
 #include "menu_2.c"
